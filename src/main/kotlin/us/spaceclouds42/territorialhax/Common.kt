714338a59dc46f72dcb02972e6d9564330be1512
SpaceClouds42 <spaceclouds42@gmail.com>
2021-04-11 17:12:52 -0400

    init

---
+++ src/main/kotlin/us/spaceclouds42/territorialhax/Common.kt
@@ -0,0 +1,10 @@
+package us.spaceclouds42.territorialhax
+
+import net.fabricmc.api.ModInitializer
+
+object Common : ModInitializer {
+    override fun onInitialize() {
+        println("Hello World")
+    }
+}
+
